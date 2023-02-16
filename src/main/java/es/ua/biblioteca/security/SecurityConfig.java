4d880a32871645944fd1d2defe18b548f5020466
hibernator11 <gustavo.candela@cerrvantesvirtual.com>
2023-02-16 18:44:23 +0100

    biblioteca subir version

--- src/main/java/es/ua/biblioteca/security/SecurityConfig.java
+++ src/main/java/es/ua/biblioteca/security/SecurityConfig.java
@@ -1,44 +1,47 @@
 package es.ua.biblioteca.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
  
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-    
-	@Override
-    protected void configure(HttpSecurity http) throws Exception{
-        
-        http
-		.authorizeRequests()
-			//.antMatchers("/", "/home").permitAll()
-			.antMatchers(HttpMethod.GET, "/api/book/**").permitAll()
-			.antMatchers(HttpMethod.GET, "/searchBook").permitAll()
-			.antMatchers(HttpMethod.POST, "/createBook").hasRole("ADMIN")
-			.antMatchers(HttpMethod.GET, "/createBook").hasRole("ADMIN")
-			.antMatchers(HttpMethod.POST, "/api/book/").hasRole("ADMIN")
-	        .antMatchers(HttpMethod.PUT, "/api/book/**").hasRole("ADMIN")
-	        .antMatchers(HttpMethod.DELETE, "/api/book/**").hasRole("ADMIN")
-			//.anyRequest().authenticated()
-			.and()
-		.formLogin()
-			//.loginPage("/login")
-			.permitAll()
-			.and()
-		.logout()
-			.permitAll();
+public class SecurityConfig {
+	
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		   http
+			.authorizeRequests()
+				//.antMatchers("/", "/home").permitAll()
+				.antMatchers(HttpMethod.GET, "/api/book/**").permitAll()
+				.antMatchers(HttpMethod.GET, "/searchBook").permitAll()
+				.antMatchers(HttpMethod.POST, "/createBook").hasRole("ADMIN")
+				.antMatchers(HttpMethod.GET, "/createBook").hasRole("ADMIN")
+				.antMatchers(HttpMethod.POST, "/api/book/").hasRole("ADMIN")
+		        .antMatchers(HttpMethod.PUT, "/api/book/**").hasRole("ADMIN")
+		        .antMatchers(HttpMethod.DELETE, "/api/book/**").hasRole("ADMIN")
+				//.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				//.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+        return http.build();
     }
-  
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-        
-        auth.inMemoryAuthentication()
-        .withUser("user").password("{noop}password").roles("USER")
-        .and()
-        .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
-    }
+	
+	 @Bean
+	 public InMemoryUserDetailsManager userDetailsService() {
+	     UserDetails user = User.withDefaultPasswordEncoder()
+	         .username("user")
+	         .password("password")
+	          .roles("ADMIN")
+	          .build();
+	     return new InMemoryUserDetailsManager(user);
+	 }
 }
